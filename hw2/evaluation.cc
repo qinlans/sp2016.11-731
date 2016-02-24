e66448f607ebfdf7f1278db17a03c99828d93449
Qinlan Shen <qinlans@cs.cmu.edu>
2016-02-24 16:53:25 -0500

    Added core evaluation file

---
+++ hw2/evaluation.cc
@@ -0,0 +1,35 @@
+#include "cnn/nodes.h"
+#include "cnn/cnn.h"
+#include "cnn/training.h"
+#include "cnn/timing.h"
+#include "cnn/rnn.h"
+#include "cnn/gru.h"
+#include "cnn/lstm.h"
+#include "cnn/dict.h"
+#include "cnn/expr.h"
+
+#include <algorithm>
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <boost/archive/text_iarchive.hpp>
+#include <boost/archive/text_oarchive.hpp>
+
+using namespace std;
+using namespace cnn;
+
+class Sentence {
+    Expression syn;
+    Expression sem;
+    float BLEU;
+    float meteor;
+};
+
+class Instance {
+    Sentence hyp1;
+    Sentence hyp2;
+    Sentence ref;
+    int correct;
+};
