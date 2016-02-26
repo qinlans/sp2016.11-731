877bf296b223b7151494d7fb25f2365e90e1bd5b
Qinlan Shen <qinlans@cs.cmu.edu>
2016-02-26 16:38:30 -0500

    Updated computation graph stuff to deal with vector of floats in an Instance

--- hw2/evaluation.cc
+++ hw2/evaluation.cc
@@ -20,15 +20,17 @@
 using namespace std;
 using namespace cnn;
 
-unsigned INPUT_DIM = 0;
+unsigned GLOVE_DIM = 50;
+unsigned SENTENCE_DIM = 100;
+unsigned INPUT_DIM = GLOVE_DIM * GLOVE_DIM + SENTENCE_DIM;
 unsigned HIDDEN_DIM = 45;
 unsigned PAIRWISE_DIM = 100;
 unsigned OUTPUT_DIM = 1;
 
 class Sentence {
   public:
-    Expression syn;
-    Expression sem;
+    vector<float> syn;
+    vector<vector<float>> sem;
     float BLEU;
     float meteor;
 };
@@ -54,9 +56,29 @@ Expression buildComputationGraph(Instance instance,
   Expression b = parameter(cg, m.add_parameters({1}));
 
   // Create embedding from syntax and semantic vector
-  Expression x1 = input_embed * concatenate({instance.hyp1.syn, instance.hyp1.sem});
-  Expression x2 = input_embed * concatenate({instance.hyp2.syn, instance.hyp2.sem});
-  Expression xref = input_embed * concatenate({instance.ref.syn, instance.ref.sem});
+  Expression hyp1syn = input(cg, {SENTENCE_DIM, 1}, instance.hyp1.syn);
+  Expression hyp2syn = input(cg, {SENTENCE_DIM, 1}, instance.hyp2.syn);
+  Expression refsyn = input(cg, {SENTENCE_DIM, 1}, instance.ref.syn);
+
+  vector<Expression> hyp1sem_vectors;
+  vector<Expression> hyp2sem_vectors;
+  vector<Expression> refsem_vectors;
+  for (int i = 0; i < instance.hyp1.sem.size(); ++i) {
+    hyp1sem_vectors.push_back(input(cg, {GLOVE_DIM, 1}, instance.hyp1.sem[i]));
+    hyp2sem_vectors.push_back(input(cg, {GLOVE_DIM, 1}, instance.hyp2.sem[i]));
+    refsem_vectors.push_back(input(cg, {GLOVE_DIM, 1}, instance.ref.sem[i])); 
+  }
+  Expression hyp1sem_matrix = concatenate_cols(hyp1sem_vectors);
+  Expression hyp2sem_matrix = concatenate_cols(hyp2sem_vectors);
+  Expression refsem_matrix = concatenate_cols(refsem_vectors);
+
+  Expression hyp1sem = reshape(hyp1sem_matrix * transpose(hyp1sem_matrix), {GLOVE_DIM * GLOVE_DIM, 1});
+  Expression hyp2sem = reshape(hyp2sem_matrix * transpose(hyp2sem_matrix), {GLOVE_DIM * GLOVE_DIM, 1});
+  Expression refsem = reshape(refsem_matrix * transpose(refsem_matrix), {GLOVE_DIM * GLOVE_DIM, 1});
+
+  Expression x1 = input_embed * concatenate({hyp1syn, hyp1sem});
+  Expression x2 = input_embed * concatenate({hyp2syn, hyp2sem});
+  Expression xref = input_embed * concatenate({refsyn, refsem});
 
   // Pairwise vectors
   Expression h12 = tanh(W12 * concatenate({x1, x2}) + b12);
